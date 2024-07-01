5d87e1e07d1816f9cf2c7a2f82e9b020a88dcb89
hklrahmadi <haikalrahmadi@kecilin.id>
2024-07-01 16:06:41 +0700

    FEAT  - create auth login and register

---
+++ app/controllers/auth_controller.ts
@@ -0,0 +1,26 @@
+import User from '#models/user'
+import { loginValidator, registerValidator } from '#validators/auth'
+import type { HttpContext } from '@adonisjs/core/http'
+
+
+
+
+export default class AuthController {
+    async login({ request, response }: HttpContext) {
+        const { email, password } = await request.validateUsing(loginValidator)
+
+        const user = await User.verifyCredentials(email, password)
+        const token = await User.accessTokens.create(user)
+        return response.ok({
+            token: token,
+            ...user.serialize()
+        })
+    }
+
+
+    async register({ request, response }: HttpContext) {
+        const payload = await request.validateUsing(registerValidator)
+        const user = await User.create(payload)
+        return response.created(user)
+    }
+}
